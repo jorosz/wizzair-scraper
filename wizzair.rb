1c9e258fa2c4714a2498d43cc8f539958d1f1017
Jozsef Orosz <jozsef@orosz.name>
2015-11-12 18:03:57 +0000

    Added multi-days support

--- wizzair.rb
+++ wizzair.rb
@@ -67,6 +67,34 @@ class WizzScraper
        end
    end
 
+  # Parses the Wizz page for the view state and secret name/value pair
+  def grep_view_state(page_source)
+    body = Nokogiri::HTML(page_source)
+    pageToken=body.at_css('form#SkySales input[name="pageToken"]').next
+    viewState=body.at('input#viewState')['value']
+    return viewState, pageToken['name'],  pageToken['value']
+  end 
+  
+  
+  # Parses Wizz page for the flight table and yields each record to a block 
+  def grep_flights(page_source)
+    table_body = Nokogiri::HTML(page_source)
+    table_body.css("label.flight.flight-data.flight-fare.flight-fare-type--basic.flight-fare--wdc").each do |label|
+      # Label contains the price and the checkbox has the flight parameters
+      date_str = label.at_css('input')['value']
+      if date_str then
+        date_arr = date_str.split '~'
+        entry = {
+          'sample_time' => DateTime.now.strftime,
+          'amount' => label.xpath('text()').text.match(/\d+(\.\d+)?/mi)[0].to_s,
+          'departure' => DateTime.strptime(date_arr[12], '%m/%d/%Y %H:%M').strftime,
+          'from_airport' => date_arr[11],
+          'to_airport' => date_arr[13]
+        }        
+        yield entry
+      end
+    end
+  end
 
   # Search using the main page
   def search(origin, destination, departure_date, return_date)
@@ -76,15 +104,13 @@ class WizzScraper
     raise 'error getting homepage' unless response.is_a? Net::HTTPSuccess
     
     # We have a GET response, now we will parse the body of the search page for hidden fields
-    body = Nokogiri::HTML(response.body)
-    pageToken=body.at_css('form#SkySales input[name="pageToken"]').next
-    viewState=body.at('input#viewState')['value']
+    view_state, secret_name, secret_value = grep_view_state(response.body)
     
     # Assemble the data to be posted
     post = {
       "__EVENTTARGET" => "HeaderControlGroupRibbonSelectView_AvailabilitySearchInputRibbonSelectView_ButtonSubmit",
-			"__VIEWSTATE" => viewState,
-      pageToken['name'] => pageToken['value'],
+			"__VIEWSTATE" => view_state,
+      secret_name => secret_value,
       "ControlGroupRibbonAnonNewHomeView$AvailabilitySearchInputRibbonAnonNewHomeView$OriginStation" => origin,
   		"ControlGroupRibbonAnonNewHomeView$AvailabilitySearchInputRibbonAnonNewHomeView$DestinationStation" => destination,
       "ControlGroupRibbonAnonNewHomeView$AvailabilitySearchInputRibbonAnonNewHomeView$DepartureDate" => departure_date,
@@ -103,30 +129,53 @@ class WizzScraper
     response=do_http("https://wizzair.com/en-GB/FlightSearch",:post,post,headers)
     raise 'error getting search results' unless response.is_a? Net::HTTPSuccess
     
-    # In the response body there is the price table where we locate the label elements
-    table_body = Nokogiri::HTML(response.body)
-    table_body.css("label.flight.flight-data.flight-fare.flight-fare-type--basic.flight-fare--wdc").each do |label|
-      # Label contains the price and the checkbox has the flight parameters
-      date_str = label.at_css('input')['value']
-      if date_str then
-        date_arr = date_str.split '~'
-        entry = {
-          'sample_time' => DateTime.now.strftime,
-          'amount' => label.xpath('text()').text.match(/\d+(\.\d+)?/mi)[0].to_s,
-          'departure' => DateTime.strptime(date_arr[12], '%m/%d/%Y %H:%M').strftime,
-          'from_airport' => date_arr[11],
-          'to_airport' => date_arr[13]
-        }
-        
+    # Parse response
+    grep_flights(response.body) do |entry|        
         yield entry if block_given?
         result << entry
-      end
     end
   
     return result
   end
   
-  private :do_http
+  def search_multiple_days(origin, destination, start_date, days = 1)
+    result = []
+
+    # Kickoff with a simple search for one day
+    search origin, destination, start_date, start_date do |entry|
+      yield entry if block_given?
+      result << entry
+    end
+    
+    
+    # Now fire off navigation requests using further POST's to select-resource
+    headers = {
+      "Referer" => "https://wizzair.com/en-GB/Select",
+      "X-Requested-With" => "XMLHttpRequest"
+    }
+    
+    post = {
+      "isAjaxRequest"=>"true",
+      "marketIndex"=>"0",
+      "direction"=>"1"
+    }
+    
+    # Step number of days - 1 (we've done the first one!)
+    (days - 1).times do
+      response = do_http("https://wizzair.com/en-GB/Select-resource",:post,post,headers)
+      return result unless response.is_a? Net::HTTPSuccess # Quit if something goes wrong
+      
+      grep_flights(response.body) do |entry|        
+        yield entry if block_given?
+        result << entry
+      end
+    end
+        
+    return result
+  end
+  
+  
+  private :do_http, :grep_flights, :grep_view_state
   attr_accessor :debug
 
 end
